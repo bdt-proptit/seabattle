27822df9e77fc96da0c93d38afec4cbc8fa493ad
PhQuangMinh <phamquangminh15012004@gmail.com>
2023-12-02 00:26:56 +0700

    UML + MVP

---
+++ Phạm Quang Minh/Product/src/MakeColor.java
@@ -0,0 +1,10 @@
+public class MakeColor {
+    public static final String backgoundRed = "\u001B[41m";
+    public static final String backgoundPurple = "\u001B[45m";
+    public static final String backgoundYellow = "\u001B[43m";
+    public static final String blue = "\u001B[34m";
+    public static final String yellow = "\u001B[43m";
+    public static final String red = "\u001B[31m";
+    public static final String green = "\u001B[32m";
+    public static final String reset = "\u001B[0m";
+}
---
+++ Phạm Quang Minh/UML/README.md
@@ -0,0 +1,4 @@
+# Class Diagram
+![Alt text](z4918258193784_f70bb7fe84e1c384464e58d6ac2d286b.jpg)
+# Activity Diagram
+![Alt text](z4918258492947_90007050c0e62a287e46960c061dd709.jpg)
