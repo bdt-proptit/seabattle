e3fdf13274080e36871527a9ba5d6bbb1ee477c4
long20102004 <hoanghailonguyno@gmail.com>
2023-12-04 17:48:44 +0700

    update UML

--- Hoàng Hải Long/Product/src/src/GameState/GameState.java
+++
@@ -1,6 +0,0 @@
-package GameState;
-
-public enum GameState {
-    PLAYER1, PLAYER2, BOT;
-    public static GameState state = PLAYER1;
-}
--- Hoàng Hải Long/Product/src/src/Main/MainClass.java
+++
@@ -1,8 +0,0 @@
-package Main;
-
-public class MainClass {
-    public static void main(String[] args) throws Exception {
-        Game newGame = new Game();
-    }
-}
-
